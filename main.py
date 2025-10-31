687497c4b14beba2d523bb77c248396ece831e85
ShivanshKansal <shivanshkansal19@gmail.com>
2025-10-31 14:30:42 +0530

    feat: interpret files

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import getpass
 from repl import repl
 
 if __name__ == "__main__":
-    print(f"Hello {getpass.getuser()}! This is the Monkey programming language!")
-    print("Feel free to type in commands")
-    repl.start(sys.stdin, sys.stdout)
+    if len(sys.argv) > 1:
+        source_code = sys.argv[1]
+        with open(source_code) as f:
+            repl.interpret_file(f, sys.stdout)
+    else:
+        print(
+            f"Hello {getpass.getuser()}! This is the Monkey programming language!")
+        print("Feel free to type in commands")
+        repl.start(sys.stdin, sys.stdout)
--- repl/repl.py
+++ repl/repl.py
@@ -17,3 +17,14 @@ def start(inp: TextIO, out: TextIO) -> None:
         while tok.type != token.EOF:
             out.write(f"{tok}\n")
             tok = l.next_token()
+
+
+def interpret_file(file: TextIO, out: TextIO) -> None:
+    for line in file:
+        if not line:
+            break
+        l = lexer.Lexer(line)
+        tok = l.next_token()
+        while tok.type != token.EOF:
+            out.write(f"{tok}\n")
+            tok = l.next_token()
